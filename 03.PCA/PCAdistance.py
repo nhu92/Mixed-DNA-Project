7b84f3dfe4de72daf8195598c0bad0c0232afca7
Nan Hu <nan.hu@ttu.edu>
2023-11-03 14:33:53 -0500

    Update PCAdistance.py

--- 03.PCA/PCAdistance.py
+++ 03.PCA/PCAdistance.py
@@ -2,6 +2,7 @@ import argparse
 import pandas as pd
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
+import re
 
 def calculate_euclidean_distance(coordinates):
     distance_matrix = pdist(coordinates, 'euclidean')
@@ -9,20 +10,26 @@ def calculate_euclidean_distance(coordinates):
     np.fill_diagonal(distance_matrix, np.inf)
     return distance_matrix
 
-def find_best_matches(distance_matrix, nodes, row_names, k=3):
+def find_best_matches(distance_matrix, nodes, row_names, num_matches=3):
     best_matches = []
 
     for node in nodes:
-        distances = distance_matrix[node]
-        match_indices = np.argpartition(distances, k)[:k]
-        best_matches.append([row_names[node]] + [row_names[i] for i in match_indices])
+        matches = []
+        for row_idx, row in enumerate(distance_matrix[node]):
+            if row_idx not in nodes:
+                matches.append((row_names[row_idx], row))
+        matches = sorted(matches, key=lambda x: x[1])[:num_matches]
+        best_matches.append((row_names[node], [match[0] for match in matches]))
 
     return best_matches
 
+def remove_pattern(text):
+    return re.sub(r'_\d{4}_mean', '', text)
+
 def main():
-    parser = argparse.ArgumentParser(description='Find the best 3 distance matches for 3D coordinates.')
+    parser = argparse.ArgumentParser(description='Find the best matches for 3D coordinates.')
     parser.add_argument('input_file', help='Input CSV file with 3D coordinates.')
-    parser.add_argument('output_file', help='Output CSV file with best 3 distance matches.')
+    parser.add_argument('output_file', help='Output CSV file with the best matches.')
     args = parser.parse_args()
 
     # Read the input CSV file
@@ -38,11 +45,16 @@ def main():
     # Calculate pairwise Euclidean distances
     distance_matrix = calculate_euclidean_distance(coordinates)
 
-    # Find the best 3 distance matches for nodes
-    best_matches = find_best_matches(distance_matrix, nodes, row_names, k=3)
+    # Find the best matches for nodes (excluding the "NODE" row)
+    best_matches = find_best_matches(distance_matrix, nodes, row_names)
 
-    # Create a new DataFrame with the best 3 distance matches
-    output_df = pd.DataFrame(best_matches, columns=['Node'] + [f'Match_{i}' for i in range(1, 4)])
+    # Create a new DataFrame with the best matches
+    output_data = []
+    for node, matches in best_matches:
+        for match in matches:
+            output_data.append((remove_pattern(node), remove_pattern(match)))
+    
+    output_df = pd.DataFrame(output_data, columns=['Node', 'BestMatches'])
     output_df.to_csv(args.output_file, index=False)
 
 if __name__ == '__main__':
