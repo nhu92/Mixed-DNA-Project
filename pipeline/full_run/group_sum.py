5adf1c575b7204d8659c31a84160135052cd6b3a
Nan Hu <nan.hu@ttu.edu>
2023-12-04 14:13:12 -0600

    Update group_sum.py
    Add the removal of NODE hit from the Z axis.

--- pipeline/full_run/group_sum.py
+++ pipeline/full_run/group_sum.py
@@ -1,16 +1,20 @@
+
 import pandas as pd
 import argparse
 
 def main(input_file, output_file):
     file_path = input_file
     data = pd.read_csv(file_path)
-    
+
+    # Filter out rows where 'row_name' contains 'NODE'
+    filtered_data = data[~data['row_name'].str.contains("NODE")]
+
     # Group by the first column and sum the second column
-    grouped_sum = data.groupby('row_name')['total_value'].sum().reset_index()
-    
+    grouped_sum = filtered_data.groupby('row_name')['total_value'].sum().reset_index()
+
     # Save the grouped and summed data to a new CSV file
     grouped_sum.to_csv(output_file, index=False)
-    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a contour plot from a CSV file.")
     parser.add_argument("input_file", help="Input CSV file path")
