35edd005e0d161fd808a343318361187b765d746
Nan Hu <nan.hu@ttu.edu>
2024-02-29 16:03:40 -0600

    Update 01-folder_prep.sh

--- pipeline/testing_tools/01-folder_prep.sh
+++ pipeline/testing_tools/01-folder_prep.sh
@@ -16,7 +16,7 @@ do
 	mkdir ../test_lab/${mix}
 	cp -r ../full_run/* ../test_lab/${mix}
 	sed -i s/target_run/${mix}/g ../test_lab/${mix}/01-target.sh
-        sed -i s/.R1.fastq.gz/${mix}.R1.fastq.gz/g ../test_lab/${mix}/01-target.sh
+	sed -i s/.R1.fastq.gz/${mix}.R1.fastq.gz/g ../test_lab/${mix}/01-target.sh
 	sed -i s/.R2.fastq.gz/${mix}.R2.fastq.gz/g ../test_lab/${mix}/01-target.sh
 	sed -i s/proj_name=/proj_name=${mix}/g ../test_lab/${mix}/01-target.sh
 done < mix_namelist.txt
